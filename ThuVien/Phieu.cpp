34e15c5dcfaad7dd7af858e839d9d1da3678c4fc
tuan nguyen <proplaydota12345@gmail.com>
2019-01-07 10:22:45 +0700

    Added class Phieu

--- ThuVien/Phieu.cpp
+++ ThuVien/Phieu.cpp
@@ -1,12 +1,20 @@
 #include "Phieu.h"
 
+Phieu::Phieu() :nSoNgayMuon(0), m_DocGia("null", "null", "null") {}
+Phieu::Phieu(const DocGia& _dg,
+		const std::list<SachViet>& _dssv,
+		const std::list<SachNgoai>& _dssn,
+		const ThoiGian& _tg,
+		size_t _songay) : nSoNgayMuon(_songay), m_DocGia(_dg), lSachViet(_dssv), lSachNgoai(_dssn) {}
 
-
-Phieu::Phieu()
+size_t Phieu::phi_tre_han() const
 {
+		if (nSoNgayMuon <= SoNgayMuonToiDa)
+				return 0;
+		size_t n = nSoNgayMuon - SoNgayMuonToiDa;
+		return n * (lSachNgoai.size() * PhiSachNgoai + lSachViet.size() * PhiSachViet);
 }
-
-
-Phieu::~Phieu()
+size_t Phieu::phi_tre_han(const Phieu& p)
 {
+		return p.phi_tre_han();
 }
--- ThuVien/Phieu.h
+++ ThuVien/Phieu.h
@@ -1,20 +1,42 @@
 #ifndef _CL_PHIEU_H_
 #define _CL_PHIEU_H_
 
-class DocGia;
-class SachViet;
-class SachNgoai;
-
+#include "DocGia.h"
+#include "SachNgoai.h"
+#include "ThoiGian.h"
+#include <list>
 class Phieu
 {
 public:
-		const int NgayMax = 7;
-		const int PhiSachViet = 10000;
-		const int PhiSachNgoai = 20000;
-		Phieu();
-		~Phieu();
-private:
+		const size_t SoNgayMuonToiDa = 7;
+		const size_t PhiSachViet = 10000;
+		const size_t PhiSachNgoai = 20000;
 
+		//Constructors, Destructor
+		Phieu();
+		Phieu(const DocGia&, const std::list<SachViet>&, const std::list<SachNgoai>&, const ThoiGian&, size_t);
+		~Phieu() {}
+
+		//Setters
+		inline size_t& so_ngay_da_muon() { return nSoNgayMuon; }
+
+		//Getters
+		inline const DocGia& doc_gia() const { return m_DocGia; }
+		inline const std::list<SachViet>& sach_viet() const { return lSachViet; }
+		inline const std::list<SachNgoai>& sach_ngoai() const { return lSachNgoai; }
+		inline const ThoiGian& ngay_bat_dau() const { return m_NgayMuon; }
+		inline const ThoiGian ngay_ket_thuc() const;
+		inline size_t so_ngay_da_muon() const { return nSoNgayMuon; }
+		size_t phi_tre_han() const;
+
+		//static members
+		static size_t phi_tre_han(const Phieu&);
+private:
+		DocGia m_DocGia;
+		std::list<SachNgoai> lSachNgoai;
+		std::list<SachViet> lSachViet;
+		ThoiGian m_NgayMuon;
+		size_t nSoNgayMuon;
 };
 
 #endif
